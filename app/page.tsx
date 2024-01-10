eda28e3d45ea05edc2a81c09f02b40aefa75e892
SilviaRbgl <silviarbgl@gmail.com>
2024-01-10 10:48:41 +0100

    style: change info about me

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,23 @@ export default function Home() {
       <h2>srbgl</h2>
       <div>
         <p>
-          H, I'm SilviaRbgl, web developer and video editor. After 10 years
-          working as an audiovisual editor, both in companies and freelance, I
-          decided to change my career and studied a bootcamp as a full-stack
-          developer.
+          Hi! I’m a frontend developer and a video editor, based in Berlin.
+          After 10 years of experience as a video editor, I decided to change
+          career and on 2022 I joined a full-stack developer bootcamp to align
+          my skill set with the latest tech trends.
         </p>
+        <a href="/about">
+          <p>See more about me</p>
+          <svg
+            width="24"
+            height="24"
+            xmlns="http://www.w3.org/2000/svg"
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+          >
+            <path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z"></path>
+          </svg>
+        </a>
       </div>
 
       <div>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,6 +2,17 @@ export default function Projects() {
   return (
     <main>
       <h2>Projects</h2>
+
+      <div className="card">
+        <div className="card-details">
+          <p className="text-title">el mundo en fotogramas</p>
+          <p className="text-body">Card Details</p>
+        </div>
+        <a className="card-button" href="#link">
+          More info
+        </a>
+      </div>
+
       <div className="card">
         <div className="card-details">
           <p className="text-title">remote island expeditions</p>
@@ -11,6 +22,36 @@ export default function Projects() {
           More info
         </a>
       </div>
+
+      <div className="card">
+        <div className="card-details">
+          <p className="text-title">movies to review</p>
+          <p className="text-body">Card Details</p>
+        </div>
+        <a className="card-button" href="#link">
+          More info
+        </a>
+      </div>
+
+      <div className="card">
+        <div className="card-details">
+          <p className="text-title">hire an expert</p>
+          <p className="text-body">Card Details</p>
+        </div>
+        <a className="card-button" href="#link">
+          More info
+        </a>
+      </div>
+
+      {/* <div className="card">
+        <div className="card-details">
+          <p className="text-title">breaking bad API</p>
+          <p className="text-body">Card Details</p>
+        </div>
+        <a className="card-button" href="#link">
+          More info
+        </a>
+      </div> */}
     </main>
   );
 }
