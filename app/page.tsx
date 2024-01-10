92ed78db6f0257686a942d2b0753b3d81d8a24c2
SilviaRbgl <silviarbgl@gmail.com>
2024-01-10 10:26:43 +0100

    feat: add cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
+import Projects from "./projects/page";
+
 export default function Home() {
   return (
     <main>
       <h2>srbgl</h2>
+      <div>
+        <p>
+          H, I'm SilviaRbgl, web developer and video editor. After 10 years
+          working as an audiovisual editor, both in companies and freelance, I
+          decided to change my career and studied a bootcamp as a full-stack
+          developer.
+        </p>
+      </div>
+
+      <div>
+        <Projects />
+      </div>
     </main>
-  )
+  );
 }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,8 +1,16 @@
-
 export default function Projects() {
   return (
-   <main>
-        <h2>Projects</h2>
-   </main>
-  )
+    <main>
+      <h2>Projects</h2>
+      <div className="card">
+        <div className="card-details">
+          <p className="text-title">Card Title</p>
+          <p className="text-body">Card Details</p>
+        </div>
+        <a className="card-button" href="#link">
+          More info
+        </a>
+      </div>
+    </main>
+  );
 }
