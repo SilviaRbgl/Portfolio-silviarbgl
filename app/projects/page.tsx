2cc99bc052da6c4501b3d250461bfb2089da04b2
SilviaRbgl <silviarbgl@gmail.com>
2024-01-10 10:32:55 +0100

    feat: add cards

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,7 +4,7 @@ export default function Projects() {
       <h2>Projects</h2>
       <div className="card">
         <div className="card-details">
-          <p className="text-title">Card Title</p>
+          <p className="text-title">remote island expeditions</p>
           <p className="text-body">Card Details</p>
         </div>
         <a className="card-button" href="#link">
