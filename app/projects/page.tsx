577c081246d3021bccfa9fcb68806c2d9a8dda4e
SilviaRbgl <silviarbgl@gmail.com>
2024-01-11 11:16:13 +0100

    style: cards

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,48 +2,48 @@ export default function Projects() {
   return (
     <main>
       <h2>Projects</h2>
-
-      <div className="card">
-        <div className="card-details">
-          <p className="text-title">el mundo en fotogramas</p>
-          <p className="text-body">Card Details</p>
+      <div className="cards">
+        <div className="card">
+          <div className="card-details">
+            <p className="text-title">el mundo en fotogramas</p>
+            <p className="text-body">Card Details</p>
+          </div>
+          <a className="card-button" href="#link">
+            More info
+          </a>
         </div>
-        <a className="card-button" href="#link">
-          More info
-        </a>
-      </div>
 
-      <div className="card">
-        <div className="card-details">
-          <p className="text-title">remote island expeditions</p>
-          <p className="text-body">Card Details</p>
+        <div className="card">
+          <div className="card-details">
+            <p className="text-title">remote island expeditions</p>
+            <p className="text-body">Card Details</p>
+          </div>
+          <a className="card-button" href="#link">
+            More info
+          </a>
         </div>
-        <a className="card-button" href="#link">
-          More info
-        </a>
-      </div>
 
-      <div className="card">
-        <div className="card-details">
-          <p className="text-title">movies to review</p>
-          <p className="text-body">Card Details</p>
+        <div className="card">
+          <div className="card-details">
+            <p className="text-title">movies to review</p>
+            <p className="text-body">Card Details</p>
+          </div>
+          <a className="card-button" href="#link">
+            More info
+          </a>
         </div>
-        <a className="card-button" href="#link">
-          More info
-        </a>
-      </div>
 
-      <div className="card">
-        <div className="card-details">
-          <p className="text-title">hire an expert</p>
-          <p className="text-body">Card Details</p>
+        <div className="card">
+          <div className="card-details">
+            <p className="text-title">hire an expert</p>
+            <p className="text-body">Card Details</p>
+          </div>
+          <a className="card-button" href="#link">
+            More info
+          </a>
         </div>
-        <a className="card-button" href="#link">
-          More info
-        </a>
-      </div>
 
-      {/* <div className="card">
+        {/* <div className="card">
         <div className="card-details">
           <p className="text-title">breaking bad API</p>
           <p className="text-body">Card Details</p>
@@ -52,6 +52,7 @@ export default function Projects() {
           More info
         </a>
       </div> */}
+      </div>
     </main>
   );
 }
